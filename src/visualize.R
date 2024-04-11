be9b38a4ffca6d7f55d9b2f6f849e5b5f76de917
tpellard <thomas.pellard@cnrs.fr>
2024-04-11 15:38:27 +0200

    Update fig_bounds.pdf, fig_nodeprobs.pdf, and 4 more files...

--- src/visualize.R
+++ src/visualize.R
@@ -4,13 +4,15 @@ library(knitr)
 
 theme_set(
   theme_minimal(base_family = "Noto Sans", base_size = 10) +
-    theme(legend.position = "bottom", aspect.ratio = .618)
+    theme(axis.title.y = element_text(face = "italic"),
+          legend.position = "bottom", 
+          aspect.ratio = .618)
 )
-wdt <- 18
+wdt <- 14
 hgt <- wdt * .7
 
 
-bounds_byt_tb <- write_csv(here("output/results/bounds_byt_tb.csv"))
+bounds_byt_tb <- read_csv(here("output/results/bounds_byt_tb.csv"))
 fig_bounds <- bounds_byt_tb |>
   mutate(Delta = factor(Delta, levels = c("T", "R"))) |>
   ggplot(aes(x = t, y = value, linetype = family, color = family)) +
@@ -30,7 +32,16 @@ fig_qs <- qs_tb |>
   xlab("age (ka BP)") +
   ylab("Q") +
   scale_y_continuous(n.breaks = 10) +
-  scale_color_few("Dark") +
-  theme(axis.title.y = element_text(face = "italic"))
+  scale_color_few("Dark")
 ggsave(here("output/figs/fig_qs.pdf"), fig_qs, device = cairo_pdf, width = wdt, height = hgt, units = "cm")
 plot_crop(here("output/figs/fig_qs.pdf"))
+
+node_probs_tb <- read_csv(here("output/results/node_probs_tb.csv"))
+
+fig_nodeprobs <- node_probs_tb |> 
+  ggplot(aes(x = factor(age), y = p, group = factor(age)))+ 
+  geom_boxplot(fill = "gray90", outliers = FALSE) +
+  geom_point(position = position_jitter(seed = 0, width = .3), size = 1.5, alpha = 1, color = few_pal("Dark")(2)[1], shape = 1) +
+  xlab("age (ka BP)")
+ggsave(here("output/figs/fig_nodeprobs.pdf"), fig_nodeprobs, device = cairo_pdf, width = wdt, height = hgt, units = "cm")
+plot_crop(here("output/figs/fig_nodeprobs.pdf"))
