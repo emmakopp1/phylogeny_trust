ab91012c67bc0862b9b28551b9eed5392065bf64
tpellard <thomas.pellard@cnrs.fr>
2024-04-15 17:17:05 +0200

    Update fig_t5trueconsensus.pdf and visualize.R

--- src/visualize.R
+++ src/visualize.R
@@ -4,8 +4,9 @@ library(ggthemes)
 library(knitr)
 library(kableExtra)
 
+font <- "Noto Sans SemiCondensed"
 theme_set(
-  theme_minimal(base_family = "Noto Sans SemiCondensed", base_size = 10) +
+  theme_minimal(base_family = font, base_size = 10) +
     theme(axis.title.y = element_text(face = "italic"),
           legend.position = "bottom", 
           aspect.ratio = .618)
@@ -86,3 +87,69 @@ fig_nodeprobs <- node_probs_tb |>
   xlab("age (ka BP)")
 ggsave(here("output/figs/fig_nodeprobs.pdf"), fig_nodeprobs, device = cairo_pdf, width = wdt, height = hgt, units = "cm")
 plot_crop(here("output/figs/fig_nodeprobs.pdf"))
+
+font <- "Noto Sans Condensed"
+
+# library(phangorn)
+library(treeio)
+library(ggtree)
+t5_true <- read.newick(here("data/simulated/beast-data-sim-5/tree-sim-5.tree"))
+fig_t5true <- t5_true |> 
+  groupClade(.node = c(71, 95, 52)) |> 
+  ggtree(aes(color = group)) +
+  geom_tiplab(family = font, size = 9/.pt) +
+  geom_rootedge(.25) +
+  # geom_nodelab(aes(label = node)) +
+  scale_color_manual(values = c("black", few_pal("Dark")(8)), na.value="black") +
+  coord_cartesian(clip = "off", expand = FALSE) +
+  theme(plot.margin = margin(.5, 1, .5 ,0, unit = "line"), legend.position = "none")
+fig_t5true
+
+t5_consensus <- read.newick(here("data/simulated/beast-data-sim-5/consensus-5.tree"))
+t5_consensus$root.edge.length <- 0
+fig_t5consensus <- t5_consensus |> 
+  groupClade(.node = c(52, 69, 53), overlap = "original") |> 
+  ggtree(aes(color = group), ladderize = FALSE) +
+  geom_tiplab(family = font, size = 9/.pt) +
+  geom_rootedge(.25) +
+  # geom_nodelab(aes(label = node)) +
+  scale_color_manual(values = c(few_pal("Dark")(8)[1], "black", few_pal("Dark")(8)[2:8]), na.value="black") +
+  coord_cartesian(clip = "off", expand = FALSE) +
+  theme(plot.margin = margin(.5, 0.5, .5 , 1, unit = "line"), legend.position = "none")
+
+ggsave(here("output/figs/fig_t5trueconsensus.pdf"), fig_t5true + fig_t5consensus, device = cairo_pdf, width = wdt, height = hgt * 1.7, units = "cm")
+plot_crop(here("output/figs/fig_t5trueconsensus.pdf"))
+
+t9_consensus <- read.newick(here("data/simulated/beast-data-sim-9/consensus-9.tree"))
+t9_consensus$root.edge.length <- 0
+fig_t9consensus <- t9_consensus |> 
+  groupClade(.node = c(c(52, 80, 78, 87), c(82, 86), c(67, 72, 75, 84)), overlap = "original") |> 
+  ggtree(aes(color = group), ladderize = FALSE) +
+  geom_tiplab(aes(label=paste(node, label)), family = font, size = 10/.pt) +
+  geom_rootedge(.25) +
+  geom_nodelab(aes(label = node)) +
+  # scale_color_few("Dark") +
+  scale_color_manual(values = c(few_pal("Dark")(8)[3], rep(few_pal("Dark")(8)[1], 4), rep(few_pal("Dark")(8)[2], 2), rep(few_pal("Dark")(8)[3], 4), few_pal("Dark")(8)[4:9]), na.value="black") +
+  coord_cartesian(clip = "off", expand = FALSE) +
+  theme(plot.margin = margin(.5, 1.5, .5 , 1, unit = "line"), legend.position = "none")
+fig_t9consensus
+
+st_consensus <- read.newick(here("data/real/sino-tibet-ctmc-strict-bd-fossilsRemoved/sino-tibetan-ctmc-strict-bd-consensus.tree"))
+st_consensus$root.edge.length <- 0
+fig_stconsensus <- ggtree(st_consensus) +
+  geom_tiplab(family = font, size = 10/.pt) +
+  geom_rootedge(.25) +
+  coord_cartesian(clip = "off", expand = FALSE, xlim = c(-.25,11)) +
+  theme(plot.margin = margin(.5, 0, .5 ,0, unit = "line"))
+ggsave(here("output/figs/fig_stconsensus.pdf"), fig_stconsensus, device = cairo_pdf, width = wdt, height = hgt * 2, units = "cm")
+plot_crop(here("output/figs/fig_stconsensus.pdf"))
+
+ie_consensus <- read.newick(here("data/real/IECoR-ctmc-strict-fbd/IECoR2-chr_consensus.tree"))
+ie_consensus$root.edge.length <- 0
+fig_ieconsensus <- ggtree(ie_consensus) +
+  geom_tiplab(family = font, size = 9/.pt) +
+  geom_rootedge(.25) +
+  coord_cartesian(clip = "off", expand = FALSE, xlim = c(-.25,9.35)) +
+  theme(plot.margin = margin(.5, 0, .5 ,0, unit = "line"))
+ggsave(here("output/figs/fig_ieconsensus.pdf"), fig_ieconsensus, device = cairo_pdf, width = wdt, height = hgt * 2, units = "cm")
+plot_crop(here("output/figs/fig_ieconsensus.pdf"))
