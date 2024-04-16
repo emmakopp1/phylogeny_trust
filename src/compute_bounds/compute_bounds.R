1b102b0f5ec87f54ac21c05a7e6f9c0f9d598fc2
tpellard <thomas.pellard@cnrs.fr>
2024-04-16 16:51:49 +0200

    Update bounds_byt_tb.csv and compute_bounds.R

--- src/compute_bounds/compute_bounds.R
+++ src/compute_bounds/compute_bounds.R
@@ -107,7 +107,7 @@ compute_upper_bound_topology(
 # You can do the same by using the function compute_upper_bound_root
 
 t_values <- seq(0, 20, length.out = 100)
-fams <- c("Bantu", "Bantu subset", "Bantu subset 2", "Indo-European", "Sino-tibetan", "Transeurasian")
+fams <- c("Bantu", "Bantu subset", "Bantu subset 2", "Indo-European", "Sino-Tibetan", "Transeurasian")
 bounds_byt_tb <- tibble(
   t = rep(t_values, length(fams)),
   "Delta_T" = c(f_topology_bantu(t_values), f_topology_bantu_sub(t_values), f_topology_bantu_sub2(t_values), f_topology_iecor(t_values), f_topology_st(t_values), f_topology_trans(t_values)),
