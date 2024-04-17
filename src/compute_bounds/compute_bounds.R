6b3ac0cd00c3de3424c5277770f3ca5fa50a05fe
tpellard <thomas.pellard@gmail.com>
2024-04-17 20:07:21 +0200

    Update bounds_tb.csv and compute_bounds.R

--- src/compute_bounds/compute_bounds.R
+++ src/compute_bounds/compute_bounds.R
@@ -54,16 +54,16 @@ bounds_tb <- dt_params |>
     ~ tibble(
       t = .x$t, k = .x$k, N = .x$n, pi0 = .x$pi0, pi1 = .x$pi1,
       DT = compute_upper_bound_topology(t, k, .x$Q, N),
-      DR = compute_upper_bound_root(t, .x$Q, N, pi0, pi1),
+      DS = compute_upper_bound_root(t, .x$Q, N, pi0, pi1),
       inf_topo = find_t_value(k, .x$Q, N),
       inf_root = find_t_value_root(.x$Q, N, pi0, pi1)
     )
   ) |>
   mutate(
     family = names(dt_params),
-    "Substitution model" = "CTMC",
-    "Clock model" = "strict",
-    "Tree model" = "BD",
+    # "Substitution model" = "CTMC",
+    # "Clock model" = "strict",
+    # "Tree model" = "BD",
     .before = t
   ) |> 
   arrange(family)
