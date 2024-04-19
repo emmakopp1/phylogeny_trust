5a9bdbc2fed4a28992fb51ebe47215ff1b5661e4
tpellard <thomas.pellard@cnrs.fr>
2024-04-19 12:08:52 +0200

    Update temp.R

--- src/temp.R
+++ src/temp.R
@@ -75,8 +75,8 @@ dt <- list.dirs(here("data/real"), full.names = TRUE, recursive = FALSE) |>
     } else {
       tibble(d)
     }
-  }) |> 
-  filter(!is.na(N)) |> 
+  }) |>
+  filter(!is.na(N)) |>
   mutate(d = case_when(
     str_detect(d, "^bantu.+subsample$") ~ "Bantu subset",
     str_detect(d, "^bantu.+subsample2$") ~ "Bantu subset 2",
@@ -84,45 +84,57 @@ dt <- list.dirs(here("data/real"), full.names = TRUE, recursive = FALSE) |>
     str_detect(d, "^ie") ~ "Indo-European",
     str_detect(d, "^st") ~ "Sino-Tibetan",
     str_detect(d, "^tea") ~ "Trans-Eurasian",
-  )) |> 
+  )) |>
   rename(family = d)
 
+dt_sim <- list.dirs(here("data/simulated"), full.names = TRUE, recursive = FALSE) |>
+  map_df(function(x) {
+    d <- str_remove_all(x, ".*/")
+    logfile <- list.files(x, "\\.log", full.names = TRUE)
+    nexusfile <- here("src/preprocess_simulated_data/tree-sim.nex")
+    bind_cols(tibble(d), get_all_parameters(logfile, nexusfile))
+  }) |> 
+  mutate(d = str_remove_all(d, "[^0-9]") |> as.integer()) |>
+  rename(family = d) |> 
+  arrange(family)
 
-list.dirs(here("data/simulated"), full.names = TRUE, recursive = FALSE) |> 
-  map_df(~ list.files(.x, "\\.log", full.names = TRUE) |> 
-        get_tracerlog_parameters()
-      )
+dt_sim |> 
+  select(family, pi0, pi1, t_R) |> 
+  kbl("markdown")
 
-read.nexus.data(here("data/simulated/beast-data-sim-1/ctmc-strict-bd-1.trees"))
+
+simtr <- treeio::read.tree(here("data/simulated/beast-data-sim-10/tree-sim-10.tree"))
+max(castor::get_all_pairwise_distances(simtr)[, Ntip(simtr)+1], na.rm = TRUE)
+
+read.nexus(here("data/simulated/beast-data-sim-1/ctmc-strict-bd-1.trees"))
 
 
 library(kableExtra)
 clnms <- c("family", paste0("{$", c("N", "k", "\\pi_0", "\\pi_1", "q", "t_R", "\\Delta^T(t_R)", "\\Delta^S(t_R)", "\\inf_t\\{\\Delta^R(t) = 1\\}", "\\inf_t\\{\\Delta^S(t) = 1\\}"), "$}"))
-dt |> 
-  select(-nTrees) |> 
-  mutate(ub_DT = ifelse(ub_DT >= 1, "\\geq 1", round(ub_DT, 2))) |> 
-  mutate(ub_DS = ifelse(ub_DS >= 1, "\\geq 1", round(ub_DS, 2))) |> 
-  kbl(format = "latex", booktabs = TRUE, linesep = "", escape = FALSE, align = c("l", "r", "r", rep("S", 8)), digits = 2, col.names = clnms) |> 
+dt |>
+  select(-nTrees) |>
+  mutate(ub_DT = ifelse(ub_DT >= 1, "\\geq 1", round(ub_DT, 2))) |>
+  mutate(ub_DS = ifelse(ub_DS >= 1, "\\geq 1", round(ub_DS, 2))) |>
+  kbl(format = "latex", booktabs = TRUE, linesep = "", escape = FALSE, align = c("l", "r", "r", rep("S", 8)), digits = 2, col.names = clnms) |>
   add_header_above(c(" " = 7, "upper bound" = 2, "threshold age" = 2), line = FALSE)
 
 t_values <- seq(0, 20, length.out = 101)
 
-dt |> 
-  group_by(family) |> 
-  slice(1) |> 
-  mutate(count = length(t_values)) |> 
-  uncount(count) |> 
-  mutate(t = t_values) |> 
-  ungroup() |> 
-  rowwise() |> 
-  mutate(ub_DT = compute_upperbound_DT(k, N, q, t)) |> 
-  mutate(ub_DS = compute_upperbound_DS(pi0, pi1, N, q, t)) |> 
-  mutate(ub_DT = min(1, ub_DT)) |> 
-  mutate(ub_DS = min(1, ub_DS)) |> 
-  ggplot(aes(x = t, y = ub_DS, linetype = family, color = family)) +
+dt |>
+  group_by(family) |>
+  slice(1) |>
+  mutate(count = length(t_values)) |>
+  uncount(count) |>
+  mutate(t = t_values) |>
+  ungroup() |>
+  rowwise() |>
+  mutate(ub_DT = compute_upperbound_DT(k, N, q, t)) |>
+  mutate(ub_DS = compute_upperbound_DS(pi0, pi1, N, q, t)) |>
+  mutate(ub_DT = min(1, ub_DT)) |>
+  mutate(ub_DS = min(1, ub_DS)) |>
+  ggplot(aes(x = t, y = ub_DT, linetype = family, color = family)) +
   geom_line() +
   xlab("age (ka BP)") +
   ylab("upper bound") +
   theme_minimal() +
   ggthemes::scale_color_few("Dark")
-
