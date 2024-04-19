6d540ce4a8671fa9f21ac852881a2d1ec4fb5f6f
tpellard <thomas.pellard@cnrs.fr>
2024-04-19 10:35:40 +0200

    Update temp.R

--- src/temp.R
+++ src/temp.R
@@ -88,8 +88,16 @@ dt <- list.dirs(here("data/real"), full.names = TRUE, recursive = FALSE) |>
   rename(family = d)
 
 
+list.dirs(here("data/simulated"), full.names = TRUE, recursive = FALSE) |> 
+  map_df(~ list.files(.x, "\\.log", full.names = TRUE) |> 
+        get_tracerlog_parameters()
+      )
+
+read.nexus.data(here("data/simulated/beast-data-sim-1/ctmc-strict-bd-1.trees"))
+
+
 library(kableExtra)
-clnms <- c("family", paste0("{$", c("N", "k", "\\pi_0", "\\pi_1", "q", "t_R", "\\Delta^T", "\\Delta^S", "\\inf_t\\{\\Delta^R(t) = 1\\}", "\\inf_t\\{\\Delta^S(t) = 1\\}"), "$}"))
+clnms <- c("family", paste0("{$", c("N", "k", "\\pi_0", "\\pi_1", "q", "t_R", "\\Delta^T(t_R)", "\\Delta^S(t_R)", "\\inf_t\\{\\Delta^R(t) = 1\\}", "\\inf_t\\{\\Delta^S(t) = 1\\}"), "$}"))
 dt |> 
   select(-nTrees) |> 
   mutate(ub_DT = ifelse(ub_DT >= 1, "\\geq 1", round(ub_DT, 2))) |> 
@@ -106,9 +114,9 @@ dt |>
   uncount(count) |> 
   mutate(t = t_values) |> 
   ungroup() |> 
+  rowwise() |> 
   mutate(ub_DT = compute_upperbound_DT(k, N, q, t)) |> 
   mutate(ub_DS = compute_upperbound_DS(pi0, pi1, N, q, t)) |> 
-  rowwise() |> 
   mutate(ub_DT = min(1, ub_DT)) |> 
   mutate(ub_DS = min(1, ub_DS)) |> 
   ggplot(aes(x = t, y = ub_DS, linetype = family, color = family)) +
