77e9d0289534d87bf8b5dd848b33940a40ab7a6f
tpellard <thomas.pellard@cnrs.fr>
2024-05-24 14:48:13 +0200

    Update 03_Transform.R

--- src/03_Transform.R
+++ src/03_Transform.R
@@ -1,26 +1,26 @@
 library(here)
 library(tidyverse)
-library(TreeTools)
-library(kableExtra)
-library(tracerer)
-library(ape)
-library(adephylo)
-library(phytools)
-library(castor)
+# library(TreeTools)
+# library(kableExtra)
+# library(tracerer)
+# library(ape)
+# library(adephylo)
+# library(phytools)
+# library(castor)
 
 
 # Compute the upper bound of the probability of inferring the true tree topology
-compute_upperbound_DT <- function(k, N, q, t, s=0) {
+compute_upperbound_DT <- function(k, N, q, t, s = 0) {
   k * N * exp(-q * (t - s))
 }
 
 # Compute the upper bound of the probability of correctly inferring ancestral states version2
 compute_upperbound_DS <- function(pi0, pi1, q, ages) {
-  max(pi0, pi1) + sum(exp(-q*ages))
+  max(pi0, pi1) + sum(exp(-q * ages))
 }
 
 compute_upperbound_DS2 <- function(par, ages) {
-  max(pi0, pi1) + sum(exp(-q*ages))
+  max(pi0, pi1) + sum(exp(-q * ages))
 }
 
 # Compute the time threshold beyond which the upper bound of the probability
@@ -33,26 +33,46 @@ compute_inf_t_DT <- function(k, N, q, t, interval = c(0, 20), tol = 1e-6, maxite
 
 # Compute the time threshold beyond which the upper bound of the probability
 # of correctly inferring ancestral states falls below 1
-compute_inf_t_DS <- function(pi0, pi1, N, q, t, interval = c(0, 20), tol = 1e-6, maxiter = 1000) {
+compute_inf_t_DS <- function(pi0, pi1, q, t, interval = c(0, 20), tol = 1e-6, maxiter = 1000) {
   uniroot(function(t) {
-    compute_upperbound_DS(pi0, pi1, N, q, t) - 1
+    compute_upperbound_DS(pi0, pi1, q, t) - 1
   }, interval = interval, tol = tol, maxiter = maxiter)$root
 }
 
 
+tipages_summary <- read_csv(here("output/results/tipages_summary.csv"))
+
+tracelog_summary <- read_csv(here("output/results/tracelog_summary.csv"))
+
+tracelog_summary |>
+  rowwise() |> 
+  mutate(ub_DT = compute_upperbound_DT(k, N, q, t_R), 
+         inf_t_DT = compute_inf_t_DT(k, N, q, t_R),
+         ub_DS = compute_upperbound_DS(pi0, pi1, q, ages = filter(tipages_summary, family == family)$age),
+         inf_t_DS = compute_inf_t_DS(pi0, pi1, q, t = filter(tipages_summary, family == family)$age)
+  )
+
+# tipages_summary |> 
+#   right_join(tracelog_summary) |> 
+#   group_by(family) |> 
+#   group_map( ~
+#     
+#     .y
+#   )
+  
+
 
 dt_real_ages <- list.dirs(here("output/results"), full.names = TRUE, recursive = FALSE) %>%
   map_df(function(x) {
-    ages <- list.files(list.dirs(here("output/results"), full.names = TRUE, recursive = FALSE), "tipages", full.names = TRUE)%>%
-      read_csv()%>%
-      select(age)%>%
+    ages <- list.files(list.dirs(here("output/results"), full.names = TRUE, recursive = FALSE), "tipages", full.names = TRUE) %>%
+      read_csv() %>%
+      select(age) %>%
       tibble()
     parameters <- read.csv(here("output/results/bounds_real_tb.csv"))
-    
   })
 
 # Find the extension of a file
-path_extension <- function(path){
+path_extension <- function(path) {
   tolower(substr(path, nchar(path) - 3, nchar(path)))
 }
 
@@ -61,18 +81,14 @@ path_extension <- function(path){
 tipages_files <- list.files(list.dirs(here("output/results"), full.names = TRUE, recursive = FALSE), "tipages", full.names = TRUE)
 
 # Fonction qui lit les fichiers d'ages et sort les âges
-compute_ages<- function(x){
-  if (path_extension(x)==".csv") {
-    read_csv(x, col_types = cols()) %>% 
+compute_ages <- function(x) {
+  if (path_extension(x) == ".csv") {
+    read_csv(x, col_types = cols()) %>%
       select(age)
   } else {
-    readRDS(x) %>% 
-      select(age) 
+    readRDS(x) %>%
+      select(age)
   }
 }
 
-map_df(tipages_files, ~compute_ages(.x)) #marche pas il ne fait pas de bind_rows()
-
-
-
-
+map_df(tipages_files, ~ compute_ages(.x)) # marche pas il ne fait pas de bind_rows()
