711c5b3c4933c4fdcde58b768bc402152eca1f0c
tpellard <thomas.pellard@cnrs.fr>
2024-05-28 15:24:37 +0200

    Update iecor.nex, iecor_old.nex, and 01_Import.R On n'a plus besoin du fichier iecor.nex modifié à la main, on prend l'original tel quel

--- src/01_Import.R
+++ src/01_Import.R
@@ -86,7 +86,7 @@ write_csv(ages_ie, bzfile(here("output/results/ie/iecor_ctmc-strict-M1_tipages.c
 trace_ie <- parse_beast_tracelog_file(here("data/real/iecor_ctmc-strict-M1/iecor_ctmc-strict-M1.log"))
 write_csv(trace_ie, here("output/results/ie/iecor_ctmc-strict-M1.csv"))
 
-ntipschars_ie <- get_nexus_parameters(here("data/real/iecor_ctmc-strict-M1/iecor_old.nex")) |>
+ntipschars_ie <- get_nexus_parameters(here("data/real/iecor_ctmc-strict-M1/iecor.nex")) |>
   mutate(family = "IE")
 
 
