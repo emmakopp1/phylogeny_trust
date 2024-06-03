669af09357cc08d4fd60d298533623a45d36198a
Emma <emma.kopp@dauphine.psl.eu>
2024-06-03 12:29:16 +0200

    show bug

--- src/03_Transform.R
+++ src/03_Transform.R
@@ -1,12 +1,5 @@
 library(here)
 library(tidyverse)
-# library(TreeTools)
-# library(kableExtra)
-# library(tracerer)
-# library(ape)
-# library(adephylo)
-# library(phytools)
-# library(castor)
 
 
 # Compute the upper bound of the probability of inferring the true tree topology
@@ -16,12 +9,13 @@ compute_upperbound_DT <- function(k, N, q, t, s = 0) {
 
 # Compute the upper bound of the probability of correctly inferring ancestral states version2
 compute_upperbound_DS <- function(pi0, pi1, q, ages) {
-  max(pi0, pi1) + sum(exp(-q * ages))
+  max(pi0, pi1) + sum(exp(-as.numeric(q) * ages))
 }
 
-compute_upperbound_DS2 <- function(par, ages) {
-  max(pi0, pi1) + sum(exp(-q * ages))
+compute_upperbound_DS2 <- function(pi0, pi1, q, t, N) {
+  max(pi0, pi1) + N*exp(-q * t)
 }
+
 
 # Compute the time threshold beyond which the upper bound of the probability
 # of inferring the true tree topology falls below 1
@@ -33,9 +27,9 @@ compute_inf_t_DT <- function(k, N, q, t, interval = c(0, 20), tol = 1e-6, maxite
 
 # Compute the time threshold beyond which the upper bound of the probability
 # of correctly inferring ancestral states falls below 1
-compute_inf_t_DS <- function(pi0, pi1, q, t, interval = c(0, 20), tol = 1e-6, maxiter = 1000) {
+compute_inf_t_DS <- function(pi0, pi1, q, N, interval = c(0, 20), tol = 1e-6, maxiter = 1000) {
   uniroot(function(t) {
-    compute_upperbound_DS(pi0, pi1, q, t) - 1
+    compute_upperbound_DS2(pi0,pi1,q,t,N) - 1
   }, interval = interval, tol = tol, maxiter = maxiter)$root
 }
 
@@ -48,15 +42,17 @@ bounds_real_tb <- tracelog_summary |>
   relocate(c(N, k), .after = n_trees) |> 
   rowwise() |>
   mutate(
-    ub_DT = compute_upperbound_DT(k, N, q, t_R),
+    #ub_DT = compute_upperbound_DT(k, N, q, t_R),
+    #ub_DS = compute_upperbound_DS(pi0, pi1, q, ages = filter(tipages_summary, family == family)$age, N),
     ub_DS = compute_upperbound_DS(pi0, pi1, q, ages = filter(tipages_summary, family == family)$age),
-    inf_t_DT = compute_inf_t_DT(k, N, q, t_R),
-    inf_t_DS = compute_inf_t_DS(pi0, pi1, q, t = filter(tipages_summary, family == family)$age)
+    #inf_t_DT = compute_inf_t_DT(k, N, q, t_R),
+    inf_t_DS = compute_inf_t_DS(pi0, pi1, q, N)
   )
+
 
 write_csv(bounds_real_tb, here("output/results/bounds_real_tb.csv"))
 
-
+tt
 
 # dt_real_ages <- list.dirs(here("output/results"), full.names = TRUE, recursive = FALSE) %>%
 #   map_df(function(x) {
