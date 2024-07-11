3b815dadbee9e05e029f6e8665295b1d81e5865b
Emma <emma.kopp@dauphine.psl.eu>
2024-07-11 11:10:21 +0200

    finish survival prob

--- src/cognat_survival_prob/functions.R
+++ src/cognat_survival_prob/functions.R
@@ -9,59 +9,6 @@ library(tidyr)
 
 # ----------- Probabilty of survival in both subtrees induces by the root -----------
 
-# This function output the branch length in a tree between two nodes
-find_branch_length <- function(tree, father, children) {
-  indice <- which(tree$edge[, 1] == father & tree$edge[, 2] == children)
-  return(tree$edge.length[indice])
-}
-
-# This function outputs the probability of a cognat to survive between node i to node j 
-delta <- function(tree, i, j, mu= 0.2052587) {
-  return(exp(-mu * find_branch_length(tree, i, j)))
-}
-
-
-# See notations in article Gray&Nicholls 2018
-# Sequence u
-u <- function(tree, i, mu= 0.2052587) {
-  if (i %in% 1:(tree$Nnode + 1)) {
-    return(0)
-  } 
-  else {
-    childrens <- Descendants(tree, i, type = c("children"))
-    return(
-      (1 - delta(tree, i, childrens[1], mu) + delta(tree, i, childrens[1],mu) * u(tree, childrens[1], mu)) *
-        (1 - delta(tree, i, childrens[2], mu) + delta(tree, i, childrens[2], mu) * u(tree, childrens[2], mu))
-    )}}
-
-# Probability of a trait born in i to survive in 1 or more leave
-P <- function(tree, i, mu= 0.2052587) {
-  return(1 - u(tree, i, mu))
-}
-
-
-# Prob of a trait born in i to survive in bot subtree
-Q <- function(tree, i, mu= 0.2052587) {
-  childrens <- Descendants(tree, i, type = c("children"))
-  return(
-    delta(tree, i, childrens[1], mu) * P(tree, childrens[1], mu) *
-      delta(tree, i, childrens[2], mu) * P(tree, childrens[2], mu)
-  )
-}
-
-
-# Separate the tip of tree into two set regarding to the subtrees induced by the root
-tips_of_subtree = function(tree){
-  root = find_root(tree)
-  child1 = Descendants(tree, root, type = c("children"))[[1]]
-  
-  tipsA = tree$tip.label[Descendants(tree, child1, type = c("tips"))[[1]]]
-  tipsB = setdiff(tree$tip.label,tipsA)
-  
-  return(list(A=tipsA, B=tipsB))
-} 
-
-
 # From .txt file to dataframe
 data_to_df = function(path){
   # Checking
