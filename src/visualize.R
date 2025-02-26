e3878199d816249208a1bf9ed87c216bcc86a0bb
Emma <emma.kopp@dauphine.psl.eu>
2025-02-26 10:34:09 +0100

    mcc tree and mirror plot

--- src/visualize.R
+++ src/visualize.R
@@ -216,136 +216,25 @@ plot_crop(here("output/figs/ancr_st_1000.pdf"))
 #ggsave(here("output/figs/fig_nodeprobs.pdf"), fig_nodeprobs, device = cairo_pdf, width = wdt, height = hgt, units = "cm")
 #plot_crop(here("output/figs/fig_nodeprobs.pdf"))
 
-#font <- "Noto Sans Condensed"
-
-
+# cophylogeny and plot 
+# true tree for 5 millenia simulation tree
 plt_dark <- few_pal("Dark")(8)
-
+ 
 t5_true <- read.newick(here("data/simulated/beast-data-sim-5/tree-sim-5.tree"))
-clds <- c(71, 95, 52)
-clds_color <- tibble(clade = factor(0:2), ca = clds, color = plt_dark[1:3])
-clds_nds <- map_df(clds, ~ tibble(ca = .x, node = unlist(Descendants(t5_true, .x, type = "tips")))) |>
-  arrange(node) |>
-  mutate(label = t5_true$tip.label) |>
-  left_join(clds_color)
-rn <- getRoot(t5_true)
+t5_mcc <- read.newick(here("data/simulated/beast-data-sim-5/mcc-5.tree"))
 
-fig_t5true <- t5_true |>
-  groupClade(.node = c(95, 52)) |>
-  full_join(clds_nds) |>
-  ggtree() +
-  geom_rootedge(.25) +
-  geom_tree(aes(color = group)) +
-  geom_tree(data = ~ filter(.x, group == 0 & x < .5)) +
-  geom_tiplab(aes(color = clade), family = font, size = 9 / .pt) +
-  scale_color_few("Dark") +
-  coord_cartesian(clip = "off", expand = FALSE) +
-  theme(plot.margin = margin(.5, 1, .5, 0, unit = "line"), legend.position = "none")
-fig_t5true
-
-t5_consensus <- read.newick(here("data/simulated/beast-data-sim-5/consensus-5.tree"))
-t5_consensus$root.edge.length <- 0
-
-fig_t5consensus <- t5_consensus |>
-  groupClade(.node = c(69, 53)) |>
-  full_join(select(clds_nds, -node)) |>
-  ggtree(ladderize = FALSE) +
-  geom_rootedge(.25) +
-  geom_tree(aes(color = group)) +
-  geom_tree(data = ~ slice(.x, 1:52)) +
-  geom_tiplab(aes(color = clade), family = font, size = 9 / .pt) +
-  # geom_nodelab(aes(label = node)) +
-  scale_color_few("Dark") +
-  coord_cartesian(clip = "off", expand = FALSE) +
-  theme(plot.margin = margin(.5, 0.5, .5, 1, unit = "line"), legend.position = "none")
-fig_t5consensus
-
-
-# miror plot 
-# true tree
-left <- rep("black",length(t5_true$tip.label))
-right <- rep("black",length(t5_consensus$tip.label))
-
-nodes1 <- Descendants(t5_true, 52, type = "tips")[[1]]
-nodes2 <- Descendants(t5_true, 71, type = "tips")[[1]]
-nodes3 <- Descendants(t5_true, 95, type = "tips")[[1]]
-
-nodes_labels1 <- t5_true$tip.label[nodes1]
-nodes_labels2 <- t5_true$tip.label[nodes2]
-nodes_labels3 <- t5_true$tip.label[nodes3]
-
-color_tip <- function(tree, nodes_labels1, nodes_labels2, nodes_labels3){
-  
-  tip_colors <- rep("black", length(tree$tip.label))
-  
-  # Colorier en fonction des ensembles
-  tip_colors[tree$tip.label %in% nodes_labels1] <- "darkblue"
-  tip_colors[tree$tip.label %in% nodes_labels2] <- "darkred"
-  tip_colors[tree$tip.label %in% nodes_labels3] <- "darkgreen"
-  
-  return(tip_colors)
-}
-
-t5_consensus_tip_colors <- color_tip(t5_consensus, nodes_labels1, nodes_labels2, nodes_labels3)
-t5_true_tip_colors <- color_tip(t5_true, nodes_labels1, nodes_labels2, nodes_labels3)
-
-# miror plot of the true tree and the consensus tree
-pdf(here("output/figs/fig_t5trueconsensus.pdf"))
-par(mfrow=c(1,2))
-plot(t5_true, tip.color = t5_true_tip_colors, direction="rightwards")
-nodelabels(bg="black", cex= 0.2, frame= 'circle')
-plot(t5_consensus, tip.color = t5_consensus_tip_colors, direction="leftwards")
-nodelabels(bg="black", cex= 0.2, frame= 'circle')
+# plot 
+pdf(here("output/figs/t5_cophylo_true_mcc.pdf"))
+cophylo_t5 <- cophylo(t5_true, t5_mcc)
+plot(cophylo_t5)
 dev.off()
 
+t9_mcc <- read.newick(here("data/simulated/beast-data-sim-9/mcc-9.tree"))
 
-# cophylogeny object 
-pdf(here("output/figs/t5_cophylo.pdf"))
-cophylo_tree <- cophylo(t5_true, t5_consensus)
-#t5_true_cophylo <- cophylo_tree$trees[[1]]
-#t5_consensus_cophylo <- cophylo_tree$trees[[2]]
-plot(cophylo_tree)
-dev.off()
-
-
-# consensus tree for 9 millenia
-t9_consensus <- read.newick(here("data/simulated/beast-data-sim-9/consensus-9.tree"))
-t9_consensus$root.edge.length <- 0
-
-fig_t9consensus <- t9_consensus |>
-  groupClade(.node = c(c(52, 80, 78, 87), c(82, 86), c(67, 72, 75, 84))) |>
-  full_join(select(clds_nds, -node)) |>
-  ggtree(ladderize = FALSE, color = "gray") +
-  geom_tree(data = ~ filter(.x, group %in% 5:6 | x < 1), color = plt_dark[2]) +
-  geom_tree(data = ~ filter(.x, group %in% 7:10 | x == 0), color = plt_dark[3]) +
-  geom_tree(data = ~ filter(.x, group %in% 1:4 | x < 1.9), color = plt_dark[1]) +
-  geom_tree(aes(color = clade)) +
-  geom_tiplab(aes(color = clade), family = font, size = 9 / .pt) +
-  geom_rootedge(.25) +
-  # geom_nodelab(aes(label = paste(node, round(x,2)))) +
-  scale_color_few("Dark") +
-  coord_cartesian(clip = "off", expand = FALSE) +
-  # theme_minimal() +
-  theme(plot.margin = margin(.5, 1.5, .5, 1, unit = "line"), legend.position = "none")
-fig_t9consensus
-
-t9_consensus_tip_colors <- color_tip(t9_consensus, nodes_labels1, nodes_labels2, nodes_labels3)
-
-# miror plot of the true tree and the consensus tree
-pdf(here("output/figs/fig_t9trueconsensus.pdf"))
-par(mfrow=c(1,2))
-plot(t5_true, tip.color = t5_true_tip_colors, direction="rightwards")
-nodelabels(bg="black", cex= 0.2, frame= 'circle')
-plot(t9_consensus, tip.color = t9_consensus_tip_colors, direction="leftwards")
-nodelabels(bg="black", cex= 0.2, frame= 'circle')
-dev.off()
-
-# cophylogeny object 
-pdf(here("output/figs/t9_cophylo.pdf"))
-cophylo_tree <- cophylo(t5_true, t9_consensus)
-#t5_true_cophylo <- cophylo_tree$trees[[1]]
-#t5_consensus_cophylo <- cophylo_tree$trees[[2]]
-plot(cophylo_tree)
+# plot
+pdf(here("output/figs/t9_cophylo_true_mcc.pdf"))
+cophylo_t9 <- cophylo(t5_true, t9_mcc)
+plot(cophylo_t9)
 dev.off()
 
 # consensus tree for sino-tibetain 
