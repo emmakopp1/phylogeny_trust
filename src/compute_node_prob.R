b8327d30ae5e6a29cf2ae2d7fb82947226e0fb63
Emma <emma.kopp@dauphine.psl.eu>
2025-03-28 17:26:53 +0100

    code for topology simulation

--- src/compute_node_prob.R
+++ src/compute_node_prob.R
@@ -1,67 +1,97 @@
 library(here)
-source(here("src/utils.R"))
+library(ape)
+library(phangorn)
+library(Matrix)
+library(matrixStats)
+library(castor)
+library(here)
+library(purrr)
+library(tidyverse)
+library(phytools)
+library(TreeTools)
+library(adephylo)
+library(paleotree)
 
+# functions --------------------------------------------------------------------
+# given a tree, compute the nodes from the deepest to youngest
+get_deepest_node <- function(tree) {
+  result <- list()
+  # Recursive function
+  preorder_recursive <- function(node_index) {
+    result <<- cbind(result, node_index)
 
-# Functions
-get_deepest_node = function(tree,N){
-  ages = as.data.frame(round(dateNodes(tree),2))
-  colnames(ages)=c('ages')
-  # We don't select the root
-  return(order(ages$ages,decreasing = T)[2:(N+1)])
+    children <- tree$edge[tree$edge[, 1] == node_index, 2]
+    for (child in children) {
+      preorder_recursive(child)
+    }
+  }
+  root <- castor::find_root(tree)
+  preorder_recursive(root)
+  return(unlist(result))
 }
 
-remove_burnin = function(trees,burnin_rate){
-  n = as.numeric(length(trees))
-  return(trees[as.integer(n*burnin_rate):n])
+# remove burnin of a phylogeny
+remove_burnin <- function(phylo, burnin) {
+  M <- length(phylo)
+  phylo[ceiling(burnin * M):M]
 }
 
-myconsensus = function(trees){
-  consensus.edges(trees, consensus.tree = consensus(trees, p=.5), rooted=T)    
+# compute consensus tree
+myconsensus <- function(trees) {
+  consensus.edges(trees, consensus.tree = consensus(trees, p = .5), rooted = T)
 }
 
+# initialization of the node and the matrix of results
+path_true <- here("data/simulated-2025-03-17/beast-data-sim-1/tree-sim-1.tree")
+tree_true <- read.tree(path_true)
+nodes <- getNodesByDepth(tree_true)[-1]
+res <- matrix(NA, nrow = 17, ncol = 10)
+colnames(res) <- as.character(nodes[1:10])
 
-# Initialisation of the node and the matrix of results
-path_true = here("data/simulated/beast-data-sim-5/tree-sim-5.tree")
-tree_true = read.tree(path_true)
-nodes = get_deepest_node(tree_true,10)
-res = matrix(NA,nrow = 17,ncol=10)
-colnames(res)=as.character(nodes)
+# For all age
+common_path <- here("data/simulated-2025-03-17/")
 
-# For all age 
-common_path = here("data/simulated/")
+for (i in 1:17) {
+  # read paths
+  path_true <- sprintf("%sbeast-data-sim-%i/tree-sim-%i.tree", common_path, i, i)
+  path_phylo <- sprintf("%sbeast-data-sim-%i/ctmc-strict-bd-%i.trees", common_path, i, i)
 
-for (i in 1:17){
-  # Read path
-  path_true = sprintf("%sbeast-data-sim-%i/tree-sim-%i.tree", common_path, i, i)
-  path_phylo = sprintf("%sbeast-data-sim-%i/ctmc-strict-bd-%i.trees", common_path, i, i)
-  
-  # Load trees
-  tree_phylo = read.nexus(path_phylo)
-  tree_true = read.tree(path_true)
-  
-  # Remove burnin of the sample
-  tree_phylo = remove_burnin(tree_phylo,0.2)
-  
-  # For all nodes compute the monophylecy of a group from the simulation with truth
-  for (s in 1:10){
-    node = nodes[s]
-    childrens = Descendants(tree_true, node, type = c("tips"))[[1]]
-    res[i,s] = mean(sapply(tree_phylo, function(t) is.monophyletic(t, tree_true$tip.label[childrens])))
+  # load trees
+  tree_phylo <- read.nexus(path_phylo)
+  tree_true <- read.tree(path_true)
+
+  # remove burnin of the sample
+  tree_phylo <- remove_burnin(tree_phylo, 0.2)
+
+  # for all nodes compute the monophylecy of a group from the simulation with truth
+  for (s in 1:10) {
+    node <- nodes[s]
+    childrens <- Descendants(tree_true, node, type = c("tips"))[[1]]
+    res[i, s] <- mean(sapply(tree_phylo, function(t) is.monophyletic(t, tree_true$tip.label[childrens])))
   }
 }
 
-library(ape)
-trees = read.nexus("/Users/kopp/Documents/phylogeny_trust/data/simulated/beast-data-sim-7/ctmc-strict-bd-7.trees")
-trees = remove_burn_in(trees,0.9)
-consensus_tree = myconsensus(trees)
-write.tree(consensus_tree,"/Users/kopp/Documents/phylogeny_trust/data/simulated/beast-data-sim-7/consensus-7.tree")
+View(res)
 
 
+res.t <- as.data.frame(t(res))
+colnames(res.t) <- as.character(seq(1, 17, 1))
 
-res.t = as.data.frame(t(res))
-colnames(res.t) = as.character(seq(1,17,1))
-node_probs_tb <- as_tibble(res.t) |> 
-  pivot_longer(everything(), names_to = "age", values_to = "p") |> 
-  mutate(age = as.integer(age)) |> 
+boxplot(res.t, col = 'darkblue')
+node_probs_tb <- as_tibble(res.t) |>
+  pivot_longer(everything(), names_to = "age", values_to = "p") |>
+  mutate(age = as.integer(age)) |>
   arrange(age, p)
-write_csv(node_probs_tb, here("output/results/node_probs_tb.csv"))
+# change path
+# write_csv(node_probs_tb, here("output/results/node_probs_tb.csv"))
+
+# from visualize
+# node_probs_tb <- read_csv(here("output/results/node_probs_tb.csv"), show_col_types = FALSE)
+fig_nodeprobs <- node_probs_tb |>
+  ggplot(aes(x = factor(age), y = p, group = factor(age))) +
+  geom_boxplot(fill = "gray90", outliers = FALSE) +
+  geom_point(position = position_jitter(seed = 0, width = .3), size = 1.5, alpha = 1, color = few_pal("Dark")(2)[1], shape = 1) +
+  xlab("age (ka BP)") +
+  ylab("proportion")
+ggsave(here("output/figs/fig_nodeprobs2025-03-17.pdf"), fig_nodeprobs, device = cairo_pdf, width = wdt, height = hgt, units = "cm")
+plot_crop(here("output/figs/fig_nodeprobs2025-03-17.pdf"))
--- src/tree_simulation.R
+++ src/tree_simulation.R
@@ -1,3 +1,4 @@
+rm(list=ls())
 library(here)
 library(TreeSim)
 library(ape)
@@ -9,12 +10,13 @@ library(xml2)
 library(dplyr)
 library(beastier)
 
+# simulation of the initial tree --------------------------------------------
+path <- here(sprintf("data/simulated_temp/beast-data-sim-%s", Sys.Date()))
+dir_path <-sprintf("data/simulated-%s", Sys.Date())
+unlink(dir_path, recursive = TRUE, force = T)
+dir.create(here(dir_path))
 
-# 1. Simulation of the initial tree
-path <- here("data/simulated_temp/beast-data-sim-")
-dir.create(here("data/simulated_temp"))
-
-# Parameter initialization
+# parameter initialization
 N <- 50
 div <- 0.245
 r <- 0.293
@@ -25,7 +27,7 @@ mu <- (r * div) / (1 - r)
 psi <- (s / (1 - s)) * (r * div / (1 - r))
 t <- 1
 
-# Tree simulation
+# tree simulation
 tree <- sim.bd.taxa.age(
   n = N,
   numbsim = 1,
@@ -38,62 +40,74 @@ tree <- sim.bd.taxa.age(
 
 tree <- tree[[1]]
 
-
-# 2. Construction of the scaled trees
+# scaling ----------------------------------------------------------------------
 l <- seq(1, 17, 1)
 
 for (k in l) {
   new_tree <- tree
   new_tree$edge.length <- k * as.numeric(new_tree$edge.length)
-  dir.create(sprintf(here("data/simulated_temp/beast-data-sim-%d"), k))
-  write.tree(new_tree,
-    file = sprintf(here("data/simulated_temp/beast-data-sim-%d/tree-sim-%d.tree"), k, k)
+  dir.create(paste0(dir_path, sprintf("/beast-data-sim-%d",k)))
+  write.tree(
+    new_tree,
+    file = paste0(dir_path, sprintf("/beast-data-sim-%d/tree-sim-%d.tree", k, k))
   )
 }
 
-# Generate sequences
-files <- list.files(here("data/simulated_temp"), full.names = TRUE, recursive = TRUE)
+# generate sequences -----------------------------------------------------------
+files <- list.files(dir_path, full.names = TRUE, recursive = TRUE)
 target_text <- read_lines(here("data/beast-data-sim.xml")) %>% paste(collapse = "\n")
 
 process_file <- function(file) {
+  # get the tree
   file_text <- read_lines(file) |> paste(collapse = "\n")
   str_replace(target_text, "xyz", file_text) |>
     str_replace("output_name", sprintf("beast-simulated-seq-%d.xml", as.numeric(str_extract(file, "(\\d+)(?=\\.tree)")))) |>
     write_lines(
-      file %>%
+      file |>
         str_replace("/tree-sim-\\d+\\.tree$", "") %>% # Remove the tree file part
         str_replace("beast-data-sim-\\d+", "\\0/\\0.xml")
-    )
-}
+    )}
 
 updated_texts <- files |>
   map_chr(~ process_file(.x))
 
+
+# generate sequence with beast -------------------------------------------------
 for (i in 1:17) {
-  # Run beast to generate sequence
-  system(sprintf(("../../../../Applications/beast/bin/beast -overwrite /Users/kopp/Documents/phylogeny_trust/data/simulated_temp/beast-data-sim-%d/beast-data-sim-%d.xml"), i, i))
-  # Change the emplacement of the output
-  system(sprintf(("mv beast-simulated-seq-%d.xml  data/simulated_temp/beast-data-sim-%d/beast-simulated-seq-%d.xml"), i, i, i))
+  # run beast to generate sequence
+  system(paste0("../../../../Applications/BEAST2.6.7/bin/beast -overwrite ", dir_path, sprintf("/beast-data-sim-%d/beast-data-sim-%d.xml", i, i)))
+  # change the emplacement of the output
+  system(
+    paste0(sprintf("mv beast-simulated-seq-%d.xml ",i), "/Users/kopp/Documents/phylogeny_trust/", dir_path, sprintf("/beast-data-sim-%d/beast-simulated-seq-%d.xml",i,i))
+    )
 }
 
+# GENERATE MANUALLY A BEAST FILE
 
-# Generate BEAST file for analysis
-target_text <- read_lines(here("data/ctmc-strict-bd.xml")) |> paste(collapse = "\n")
 
-sequences <- list.files(here("data/simulated_temp"), full.names = TRUE, recursive = TRUE) |>
+# template ctmc
+# import alignement
+# calibration
+#tt <- read.tree("/Users/kopp/Documents/phylogeny_trust/data/simulated-2025-03-17/beast-data-sim-1/tree-sim-1.tree")
+#calib <- c('t22','t17','t19','t23')
+#max(node.depth.edgelength(tt)) - node.depth.edgelength(tt)[findMRCA(tt,calib)]
+
+
+# Generate BEAST file for analysis ---------------------------------------------
+sequences <- list.files(dir_path, full.names = TRUE, recursive = TRUE) |>
   keep(~ str_detect(.x, "\\.xml$")) |>
   keep(~ str_detect(.x, "simulated-seq"))
 
-beast_file <- read_xml(here("data/ctmc-strict-bd.xml"))
+beast_file <- read_xml(here(paste0(dir_path,"/beast-data-sim-1/ctmc-strict-bd-1.xml")))
 
-# Replace in .xml file the values of the generated sequences
+# replace in .xml file the values of the generated sequences
 replace_value <- function(xml_file, df, path_out) {
   xml_file |>
     xml_find_all("//sequence") |>
     walk(~ {
-      taxon <- xml_attr(.x, "taxon")
+      taxa <- xml_attr(.x, "taxon")
       new_value <- df |>
-        filter(taxon == taxon) |>
+        filter(taxon == taxa) |>
         pull(value)
       if (length(new_value) > 0) {
         xml_set_attr(.x, "value", new_value)
@@ -104,8 +118,10 @@ replace_value <- function(xml_file, df, path_out) {
   write_xml(xml_file, path_out)
 }
 
-# Appliquer la fonction
+# apply function 
 for (file in sequences) {
+  
+  # extract sequences
   taxons <- read_xml(file) |>
     xml_find_all("//sequence") |>
     map_df(~ {
@@ -115,82 +131,67 @@ for (file in sequences) {
         taxon = taxon, value = value
       )
     })
-
+  
+  # output path of the xml file
   path_out <- str_replace(
     file,
     "beast-simulated-seq-\\d+.xml",
-    paste0("ctmc-strict-bd-", str_extract(file, "\\d+"), ".xml")
+    paste0("ctmc-strict-bd-", str_extract(file, "\\d+(?=\\.)"), ".xml")
   )
-
 
   replace_value(beast_file, taxons, path_out)
 }
 
-# Modify calibrations
-beast_inputs = list.files(here("data/simulated_temp"), full.names = TRUE, recursive = TRUE) |>
+# Modify calibrations ----------------------------------------------------------
+beast_inputs = list.files(dir_path, full.names = TRUE, recursive = TRUE) |>
   keep(~ str_detect(.x, "\\.xml$")) |>
   keep(~ str_detect(.x, "ctmc"))
 
-
+# modify the prior calibration of a file. The coeficiant is in the file_path name
 modify_uniform_attributes <- function(file_path) {
   
-  scalar = as.integer(str_extract(file_path, "(?<=-)(\\d+)(?=\\.xml)"))
+  scalar <- as.integer(str_extract(file_path, "(?<=-)(\\d+)(?=\\.xml)"))
   xml_file <- read_xml(file_path)
-  uniform_node <- read_xml(file_path) |>
+  uniform_node <- xml_file |>
     xml_find_all("//distribution[contains(@id, '.prior')]//Uniform")
-
-  # Modify lower
+  
+  # Modifier lower
   current_lower <- as.numeric(xml_attr(uniform_node, "lower"))
   new_lower <- current_lower * scalar
   xml_set_attr(uniform_node, "lower", as.character(new_lower))
   
-  # Modify upper
+  # Modifier upper
   current_upper <- as.numeric(xml_attr(uniform_node, "upper"))
   new_upper <- current_upper * scalar
   xml_set_attr(uniform_node, "upper", as.character(new_upper))
-
+  
   write_xml(xml_file, file_path)
 }
 
-
+# change the .trees and .log files (fileName attribute)
 modify_filenames <- function(file_path) {
-  
   scalar <- as.integer(str_extract(file_path, "(?<=-)(\\d+)(?=\\.xml)"))
   xml_file <- read_xml(file_path)
   
-  # Modify file names
+  
   xml_find_all(xml_file, "//logger[contains(@id, 'treelog') or contains(@id, 'tracelog')]") |>
     walk(~ {
       current_fileName <- xml_attr(.x, "fileName")
       new_fileName <- str_replace(current_fileName, "\\d+", as.character(scalar))
+      #cat("Changing", current_fileName, "to", new_fileName, "\n")
       xml_set_attr(.x, "fileName", new_fileName)
     })
   
-  # Save 
   write_xml(xml_file, file_path)
+  #cat("File saved:", file_path, "\n")
 }
-
 
 # Apply functions
+walk(beast_inputs, modify_uniform_attributes)
 walk(beast_inputs, modify_filenames)
 
+# Just need to lauch the files 
 
-
-# BEAST analysis 
-for (i in 1:17) {
-  # Run beast to generate sequence
-  system(
-    sprintf(("../../../../Applications/beast/bin/beast -overwrite /Users/kopp/Documents/phylogeny_trust/data/simulated_temp/beast-data-sim-%d/ctmc-strict-bd-%d.xml"), i, i),
-    ignore.stdout = TRUE, 
-    ignore.stderr = TRUE)
-  system(sprintf(("mv ctmc-strict-bd-%d.log  data/simulated_temp/beast-data-sim-%d/ctmc-strict-bd-%d.log"), i, i, i))
-  system(sprintf(("mv ctmc-strict-bd-%d.trees  data/simulated_temp/beast-data-sim-%d/ctmc-strict-bd-%d.trees"), i, i, i))
-  system(sprintf(("rm ctmc-strict-bd-%d.xml.0state"), i))
-  system(sprintf(("rm ctmc-strict-bd-%d.xml.1state"), i))
-  system(sprintf(("rm ctmc-strict-bd-%d.xml.2state"), i))
-  system(sprintf(("rm ctmc-strict-bd-%d.xml.3state"), i))
-  system(sprintf(("rm ctmc-strict-bd-%d.xml.state"), i))
-}
 
 
 
