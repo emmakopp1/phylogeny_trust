ee8560cb45427422e6c0f917eaa3f183dd6cbde1
Thomas Pellard <thomas.pellard@cnrs.fr>
2026-06-04 14:59:43 +0200

    Update plots_tmp.R

--- plots_tmp.R
+++ plots_tmp.R
@@ -1,11 +1,22 @@
 library(tidyverse)
 library(here)
 library(ggh4x)
+library(ggrepel)
 library(khroma)
 library(knitr)
 
 base_font <- "Noto Sans Condensed"
 plt <- color("vibrant")(3)
+theme_set(
+  theme_minimal(base_family = base_font, base_size = 12) +
+    theme(
+      aspect.ratio = .618,
+      strip.text = element_text(size = 12),
+      legend.text = element_text(size = 12),
+      panel.grid.minor = element_blank(),
+      panel.grid.major = element_line(linewidth = .35)
+    )
+)
 
 N_traits <- 3000
 pi1 <- 0.94305
@@ -22,17 +33,18 @@ Q <- matrix(
   byrow = TRUE
 )
 
-shared_cognates <- readRDS(here("output/results/shared_cognates.rds"))
+shared_cognates <- readRDS(here("output/results/shared_cognates.rds")) |>
+  rename(age = tree_age, prop = value)
 shared_cognates |>
   ggplot() +
   geom_pointpath(
-    aes(x = tree_age, y = value),
+    aes(x = age, y = prop),
     color = plt[2],
     linewidth = 1,
     stroke = .1
   ) +
   geom_errorbar(
-    aes(x = tree_age, ymin = inf, ymax = sup),
+    aes(x = age, ymin = inf, ymax = sup),
     color = plt[2],
     width = .2
   ) +
@@ -40,12 +52,7 @@ shared_cognates |>
   xlab("Age of the most recent common ancestor (ka)") +
   scale_y_continuous(breaks = seq(0, 1, .1)) +
   scale_x_continuous(breaks = seq(0, 17, 1)) +
-  coord_cartesian(clip = "off") +
-  theme_minimal(base_family = base_font) +
-  theme(
-    aspect.ratio = .618,
-    panel.grid.minor = element_blank()
-  )
+  coord_cartesian(clip = "off")
 ggsave(
   here("output/figs/shared_cognate_no_homoplasie.pdf"),
   width = 12,
@@ -56,7 +63,7 @@ ggsave(
 plot_crop(here("output/figs/shared_cognate_no_homoplasie.pdf"))
 
 prop_shared_tip_pair <- readRDS(here(
-  "output/results/shared_cognate_tip_pair.pdf"
+  "output/results/shared_cognate_tip_pair.rds"
 )) |>
   rowid_to_column("age")
 
@@ -114,13 +121,9 @@ prop_shared_tip_pair |>
     )
   ) +
   coord_cartesian(clip = "off") +
-  theme_minimal(base_family = base_font) +
   theme(
-    aspect.ratio = .618,
-    panel.grid.minor = element_blank(),
     legend.position = "inside",
-    legend.justification = c(1, 1),
-    legend.text = element_text(size = 12),
+    legend.justification = c(1, 1)
   )
 
 ggsave(
@@ -132,9 +135,121 @@ ggsave(
 )
 plot_crop(here("output/figs/shared_cognate_tip_pair.pdf"))
 
-prop_true_to_cs <- read_csv(here("output/results/prop_true_to_cs.csv")) |>
-  mutate(type = "Majority-rule consensus", value = as.character(value))
+shared_cognates |>
+  mutate(type = "any pair") |>
+  bind_rows(
+    prop_shared_tip_pair |> mutate(type = "sample pair")
+  ) |>
+  ggplot() +
+  geom_segment(
+    aes(
+      x = -Inf,
+      xend = min(filter(prop_shared_tip_pair, prop <= .1)$age),
+      y = .1,
+      yend = .1
+    ),
+    linetype = "dashed",
+    color = "grey40",
+    linewidth = .35
+  ) +
+  geom_segment(
+    aes(
+      x = min(filter(prop_shared_tip_pair, prop <= .1)$age),
+      xend = min(filter(prop_shared_tip_pair, prop <= .1)$age),
+      y = .1,
+      yend = -Inf
+    ),
+    linetype = "dashed",
+    color = "grey40",
+    linewidth = .35
+  ) +
+  geom_segment(
+    aes(
+      x = -Inf,
+      xend = min(filter(prop_shared_tip_pair, prop <= .5)$age),
+      y = .5,
+      yend = .5
+    ),
+    linetype = "dotted",
+    color = "grey40",
+    linewidth = .35
+  ) +
+  geom_segment(
+    aes(
+      x = min(filter(prop_shared_tip_pair, prop <= .5)$age),
+      xend = min(filter(prop_shared_tip_pair, prop <= .5)$age),
+      y = .5,
+      yend = -Inf
+    ),
+    linetype = "dotted",
+    color = "grey40",
+    linewidth = .35
+  ) +
+  geom_segment(
+    aes(
+      x = -Inf,
+      xend = min(filter(shared_cognates, prop <= .5)$age),
+      y = .5,
+      yend = .5
+    ),
+    linetype = "twodash",
+    color = "grey40",
+    linewidth = .35
+  ) +
+  geom_segment(
+    aes(
+      x = min(filter(shared_cognates, prop <= .5)$age),
+      xend = min(filter(shared_cognates, prop <= .5)$age),
+      y = .5,
+      yend = -Inf
+    ),
+    linetype = "twodash",
+    color = "grey40",
+    linewidth = .35
+  ) +
+  geom_pointpath(
+    aes(x = age, y = prop, color = type),
+    linewidth = 1,
+    stroke = .1
+  ) +
+  geom_errorbar(
+    aes(x = age, ymin = inf, ymax = sup, color = type),
+    width = .2
+  ) +
+  scale_color_vibrant(name = NULL) +
+  ylab(
+    "Proportion of cognates shared\nbetween languages across the 1st split"
+  ) +
+  xlab("Age of the most recent common ancestor (ka BP)") +
+  scale_y_continuous(breaks = seq(0, 1, .1)) +
+  scale_x_continuous(breaks = seq(0, 17, 1)) +
+  coord_cartesian(clip = "off") +
+  theme(
+    legend.position = "inside",
+    legend.justification = c(1, 1)
+  )
+ggsave(
+  here("output/figs/shared_cognates.pdf"),
+  width = 12,
+  height = 12,
+  units = "cm",
+  device = cairo_pdf
+)
+plot_crop(here("output/figs/shared_cognates.pdf"))
 
+prop_true_to_cs <- read_csv(here("output/results/prop_true_to_cs.csv")) |>
+  mutate(type = "Consensus", value = as.character(value))
+prop_cs_to_true <- read_csv(here("output/results/prop_cs_to_true.csv")) |>
+  rename(value = exist, mean_n = n_mean) |>
+  mutate(type = "Consensus", value = as.character(value))
+prop_mcc_to_true <- read_csv(here("output/results/prop_mcc_to_true.csv")) |>
+  rename(value = exist, mean_n = n_mean) |>
+  mutate(type = "MCC", value = as.character(value))
+prop_hipstr_to_true <- read_csv(here(
+  "output/results/prop_hipstr_to_true.csv"
+)) |>
+  rename(value = exist, mean_n = n_mean) |>
+  mutate(type = "HIPSTR", value = as.character(value))
 
 bind_rows(prop_true_to_cs, prop_mcc_to_true, prop_hipstr_to_true) |>
   mutate(type = fct_inorder(type)) |>
@@ -163,14 +278,9 @@ bind_rows(prop_true_to_cs, prop_mcc_to_true, prop_hipstr_to_true) |>
   scale_color_highcontrast(reverse = TRUE) +
   coord_cartesian(clip = "off", expand = FALSE) +
   facet_wrap(~type) +
-  theme_minimal(base_family = base_font, base_size = 12) +
   theme(
-    aspect.ratio = .618,
-    panel.grid.minor = element_blank(),
-    legend.text = element_text(size = 12),
     legend.position = "bottom",
     legend.margin = margin(t = -.5, r = 0, b = 0, l = 0, unit = "lines"),
-    strip.text = element_text(size = 12)
   )
 ggsave(
   here("output/figs/barplot_prop_true_to_resume.pdf"),
@@ -180,18 +290,6 @@ ggsave(
   device = cairo_pdf
 )
 plot_crop(here("output/figs/barplot_prop_true_to_resume.pdf"))
-
-prop_cs_to_true <- read_csv(here("output/results/prop_cs_to_true.csv")) |>
-  rename(value = exist, mean_n = n_mean) |>
-  mutate(type = "Consensus", value = as.character(value))
-prop_mcc_to_true <- read_csv(here("output/results/prop_mcc_to_true.csv")) |>
-  rename(value = exist, mean_n = n_mean) |>
-  mutate(type = "MCC", value = as.character(value))
-prop_hipstr_to_true <- read_csv(here(
-  "output/results/prop_hipstr_to_true.csv"
-)) |>
-  rename(value = exist, mean_n = n_mean) |>
-  mutate(type = "HIPSTR", value = as.character(value))
 
 bind_rows(prop_cs_to_true, prop_mcc_to_true, prop_hipstr_to_true) |>
   mutate(type = fct_inorder(type)) |>
@@ -219,14 +317,9 @@ bind_rows(prop_cs_to_true, prop_mcc_to_true, prop_hipstr_to_true) |>
   scale_fill_manual(values = rev(color("high contrast")(3)[-2])) +
   coord_cartesian(clip = "off", expand = FALSE) +
   facet_wrap(~type) +
-  theme_minimal(base_family = base_font, base_size = 12) +
   theme(
-    aspect.ratio = .618,
-    panel.grid.minor = element_blank(),
-    legend.text = element_text(size = 12),
     legend.position = "bottom",
     legend.margin = margin(t = -.5, r = 0, b = 0, l = 0, unit = "lines"),
-    strip.text = element_text(size = 12)
   )
 ggsave(
   here("output/figs/barplot_prop_resume_to_true.pdf"),
@@ -236,3 +329,95 @@ ggsave(
   device = cairo_pdf
 )
 plot_crop(here("output/figs/barplot_prop_resume_to_true.pdf"))
+
+summary_data_st <- read_csv(here(
+  "output/results/ancestral_reconstruction_summary_st.csv"
+)) |>
+  mutate(family = "Sino-Tibetan") |>
+  rename(mean_outgroup = mean_sinitic)
+summary_data_ie <- read_csv(here(
+  "output/results/ancestral_reconstruction_summary_ie.csv"
+)) |>
+  mutate(family = "Indo-European")
+
+concepts <- bind_rows(summary_data_st, summary_data_ie) |>
+  filter(!is.na(mean_outgroup)) |>
+  mutate(x = mean_outgroup >= .5) |>
+  group_by(family, x) |>
+  filter(
+    mean_max_depth == max(mean_max_depth, na.rm = TRUE) |
+      mean_max_depth == min(mean_max_depth, na.rm = TRUE)
+  ) |>
+  ungroup() |>
+  mutate(sens = str_remove_all(sens, "hide "))
+
+bind_rows(summary_data_st, summary_data_ie) |>
+  ggplot() +
+  geom_point(
+    aes(x = mean_max_depth, y = mean_outgroup),
+    color = plt[2],
+    alpha = .5,
+    size = 1
+  ) +
+  geom_point(
+    data = concepts,
+    aes(x = mean_max_depth, y = mean_outgroup),
+    color = plt[1],
+    size = 1
+  ) +
+  geom_text_repel(
+    data = concepts,
+    aes(x = mean_max_depth, y = mean_outgroup, label = sens),
+    seed = 123,
+    min.segment.length = 0,
+    segment.size = .35,
+    # point.padding = .5,
+    family = base_font,
+    color = plt[1],
+    bg.color = "white",
+    bg.r = 0.05,
+    size = 10 / .pt
+  ) +
+  facet_wrap(~family, scales = "free") +
+  xlab("Mean maximum age (ka BP)") +
+  ylab("Probability of presence in\nthe early-diverging lineage")
+ggsave(
+  here("output/figs/ancestral_reconstruction_by_semantic_meaning.pdf"),
+  width = 12,
+  height = 12,
+  units = "cm",
+  device = cairo_pdf
+)
+plot_crop(here("output/figs/ancestral_reconstruction_by_semantic_meaning.pdf"))
+
+# summary_data_st |>
+#   mutate(age = floor(mean_max_depth * 2) / 2) |>
+#   group_by(age) |>
+#   arrange(sens) |>
+#   mutate(y = row_number() - 1) |>
+#   ggplot() +
+#   geom_text(aes(x = age, y = y, label = sens), size = 3, family = base_font)
+
+bind_rows(summary_data_st, summary_data_ie) |>
+  ggplot() +
+  geom_histogram(
+    aes(x = mean_max_depth),
+    fill = plt[2],
+    color = "white",
+    binwidth = .5
+  ) +
+  xlab("Mean maximum age (ka BP)") +
+  ylab("Number of concepts with\na reconstructible root") +
+  facet_wrap(~family, scales = "free")
+ggsave(
+  here("output/figs/hist_age_concepts.pdf"),
+  width = 12,
+  height = 12,
+  units = "cm",
+  device = cairo_pdf
+)
+plot_crop(here("output/figs/hist_age_concepts.pdf"))
+
+# summary_data_st |>
+#   ggplot() +
+#   geom_point(aes(x = mean_max_depth, y = n_traits))
