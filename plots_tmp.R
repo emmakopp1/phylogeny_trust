3de6ed7bbbfa088ecf2e3fe484c9df82a4c673b9
Thomas Pellard <thomas.pellard@cnrs.fr>
2026-06-19 16:50:27 +0200

    Tree plot

--- plots_tmp.R
+++ plots_tmp.R
@@ -6,6 +6,8 @@ library(khroma)
 library(knitr)
 library(patchwork)
 library(treeio)
+library(TreeTools)
+library(phangorn)
 library(ggtree)
 
 width <- 13.5
@@ -13,6 +15,7 @@ height <- 19
 base_font <- "Noto Sans Condensed"
 base_font2 <- "Noto Sans ExtraCondensed"
 plt <- color("vibrant")(3)
+plt2 <- color("highcontrast")(3)
 theme_set(
   theme_minimal(base_family = base_font, base_size = 9) +
     theme(
@@ -599,9 +602,15 @@ plot_crop(here("output/figs/number_of_traits_influence.pdf"))
 tree_cs <- read.tree(here(
   'data/simulated-2025-07-28/beast-data-sim-7/beast-data-sim-7-15/consensus-15.tree'
 ))
+labs_lex <- sort(tree_cs$tip.label)
+map_lex <- setNames(seq_along(labs_lex), labs_lex)
+tree_cs$tip.label <- paste0("T", map_lex[tree_cs$tip.label])
 tree_true <- read.tree(here(
   'data/simulated-2025-07-28/beast-data-sim-7/beast-data-sim-7-15/tree-sim-7-15.tree'
 ))
+labs_lex <- sort(tree_true$tip.label)
+map_lex <- setNames(seq_along(labs_lex), labs_lex)
+tree_true$tip.label <- paste0("T", map_lex[tree_true$tip.label])
 
 # analyse a concordant rake node in the consensus tree
 node_plausible <- 53 # in the true tree
@@ -623,13 +632,26 @@ mrca_rec <- getMRCA(tree_cs, tip_rec) # mrca in the cs tree
 
 # colors in the true tree
 # tip colors
-tip_colors_tt <- rep("black", length(tree_true$tip.label))
+tip_colors_tt <- tirep("black", length(tree_true$tip.label))
 # concordent in blue
-tip_colors_tt[descendant_plausible] <- "blue"
+tip_colors_tt[descendant_plausible] <- plt2[1]
 # discordant node in red
-tip_colors_tt[descendant_not_plausible] <- "red"
+tip_colors_tt[descendant_not_plausible] <- plt2[3]
 # reconciliable in yellow
-tip_colors_tt[descendant_rec] <- "yellow2"
+tip_colors_tt[descendant_rec] <- plt2[2]
+
+tb <- tibble(tip.label = tree_true$tip.label) |>
+  mutate(
+    type = case_when(
+      tip.label %in% tip_plausible ~ "Concordant",
+      tip.label %in% tip_not_plausible ~ "Discordant",
+      tip.label %in% tip_rec ~ "Reconcilable",
+      .default = NA
+    ) |>
+      factor(
+        levels = c("Discordant", "Reconcilable", "Concordant")
+      )
+  )
 
 # colors in the consensus tree
 tip_colors_cs <- rep("black", length(tree_cs$tip.label))
@@ -637,26 +659,66 @@ tip_colors_cs <- rep("black", length(tree_cs$tip.label))
 # identify tip position in the consensus tree
 # concordant
 tip_positions_cs <- match(tip_plausible, tree_cs$tip.label)
-tip_colors_cs[tip_positions_cs] <- "blue"
+tip_colors_cs[tip_positions_cs] <- plt2[1]
 # discordant
 tip_positions_cs_not_plausible <- match(tip_not_plausible, tree_cs$tip.label)
-tip_colors_cs[tip_positions_cs_not_plausible] <- "red"
+tip_colors_cs[tip_positions_cs_not_plausible] <- plt2[3]
 # reconciliable
 tip_positions_cs_rec <- match(tip_rec, tree_cs$tip.label)
-tip_colors_cs[tip_positions_cs_rec] <- "yellow2"
+tip_colors_cs[tip_positions_cs_rec] <- plt2[2]
 
-# true tree
-pdf(here("output/figs/plausible_node.pdf"), width = 12, height = 6)
-par(mfrow = c(1, 2))
-plot(tree_true, tip.color = tip_colors_tt, cex = 0.6)
-nodelabels(node = node_plausible, frame = 'circle', cex = 0.5)
-nodelabels(node = node_not_plausible, frame = 'circle', cex = 0.5)
-nodelabels(node = node_rec, frame = 'circle', cex = 0.5)
-
-# consensus tree
-plot(tree_cs, direction = "leftwards", tip.color = tip_colors_cs, cex = 0.6)
-nodelabels(node = mrca_plausible, frame = 'circle', cex = 0.5)
-nodelabels(node = mrca_not_plausible, frame = 'circle', cex = 0.5)
-nodelabels(node = mrca_rec, frame = 'circle', cex = 0.5)
-
-dev.off()
+tree_true_plot <- ggtree(tree_true, linewidth = .25, ladderize = FALSE) %<+%
+  tb +
+  geom_tiplab(
+    aes(label = label, color = type),
+    size = 8 / .pt,
+    # color = tip_colors_tt,
+    family = base_font
+  ) +
+  coord_cartesian(clip = "off")
+tree_cs_plot <- ggtree(tree_cs, linewidth = .25, ladderize = FALSE) %<+%
+  tb +
+  geom_tiplab(
+    aes(label = label, color = type, fill = type),
+    size = 8 / .pt,
+    hjust = 1,
+    # key_glyph = draw_key_rect,
+    family = base_font
+  ) +
+  geom_tippoint(
+    aes(color = type, fill = type),
+    color = NA,
+    shape = 22,
+    alpha = 0
+  ) +
+  scale_x_reverse() +
+  coord_cartesian(clip = "off")
+(tree_true_plot +
+  hexpand(.05) +
+  guides(color = "none") +
+  ggtitle("True tree") +
+  tree_cs_plot +
+  guides(
+    fill = guide_legend(override.aes = list(size = 5, alpha = 1))
+  ) +
+  ggtitle("Consensus tree") +
+  hexpand(.05, direction = 1) &
+  theme_void(base_family = base_font, base_size = 9) &
+  theme(
+    legend.position = "bottom",
+    legend.text = element_text(size = 9),
+    plot.title = element_text(size = 9, hjust = 0.5)
+  ) &
+  scale_color_highcontrast(reverse = TRUE, na.value = "black", guide = "none") &
+  scale_fill_highcontrast(reverse = TRUE, na.translate = FALSE) &
+  labs(color = "", fill = "")) +
+  # guide_area() +
+  plot_layout(guides = 'collect')
+ggsave(
+  here("output/figs/plausible_node.pdf"),
+  width = width,
+  height = height / 1.5,
+  units = "cm",
+  device = cairo_pdf
+)
+plot_crop(here("output/figs/plausible_node.pdf"))
